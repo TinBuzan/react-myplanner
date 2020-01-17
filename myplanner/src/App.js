b7f4aa54141333a0f17f4ef0ae1bf1f7c7f28d6c
Tin Buzancic <tinbuzan@gmail.com>
2020-01-16 21:20:15 -0500

    added navbar code

--- myplanner/src/App.js
+++ myplanner/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>
-          Tin Planner
-        </h1>
+        <Navbar />
       </div>
     </BrowserRouter>
   );
