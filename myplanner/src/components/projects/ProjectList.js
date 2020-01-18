1f32dccb1510e217694f28722fc75c2e48d95aa6
Tin Buzancic <tinbuzan@gmail.com>
2020-01-18 12:41:15 -0500

    hook up dummy data to ProjectList and project summary

--- myplanner/src/components/projects/ProjectList.js
+++ myplanner/src/components/projects/ProjectList.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ProjectSummary from './ProjectSummary';
 
-const ProjectList = () => {
+const ProjectList = ({ projects }) => {
+  const projectList = projects ? projects.map(project => {
+    return(
+      <ProjectSummary project={ project } key={ project.id }/>
+    )
+  }) : (
+    <div>There are no projects </div>
+  )
+
   return (
     <div className="project-list section">
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
+      { projectList }
     </div>
   )
 }
